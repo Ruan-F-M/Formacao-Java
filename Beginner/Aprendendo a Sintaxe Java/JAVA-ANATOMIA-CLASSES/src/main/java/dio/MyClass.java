c513e7c4782378be7f62b7bc203bd6b48adc2b86
Ruan-F-M <ruanzinr1000@hotmail.com>
2023-01-09 21:46:18 -0300

    syntax pt2

---
+++ Beginner/Aprendendo a Sintaxe Java/JAVA-ANATOMIA-CLASSES/src/main/java/dio/MyClass.java
@@ -0,0 +1,16 @@
+package dio;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String firstName = "Ruan";
+        String secondName = "França";
+
+        String fullName = fullName(firstName, secondName);
+
+        System.out.println(fullName);
+    }
+
+    public static String fullName (String firstName, String secondName) {
+        return "Resultado do método " + firstName.concat(" ").concat(secondName);
+    }
+}
--- Beginner/Aprendendo a Sintaxe Java/JAVA-ANATOMIA-CLASSES/src/main/java/dio/class/MyClass.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class MyClass {
-    public static void main(String[] args) {
-        System.out.println("Bom dia pessoal!");
-    }
-}
