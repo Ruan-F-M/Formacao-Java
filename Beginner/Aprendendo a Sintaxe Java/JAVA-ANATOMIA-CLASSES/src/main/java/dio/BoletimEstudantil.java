a756b93f292f67714d1d8796a0746ba5b211acf3
Ruan-F-M <ruanzinr1000@hotmail.com>
2023-01-10 22:19:10 -0300

    syntax pt3

---
+++ Beginner/Aprendendo a Sintaxe Java/JAVA-ANATOMIA-CLASSES/src/main/java/dio/BoletimEstudantil.java
@@ -0,0 +1,24 @@
+package dio;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        //        Codigo hidentado
+        int finalMedia = 7;
+        if (finalMedia < 6)
+            System.out.println("Failed");
+        else if (finalMedia == 6)
+            System.out.println("TEST MINERVA");
+        else
+            System.out.println("APPROVED");
+
+//         Codigo não está hidentado
+
+//        int finalMedia = 7;
+//        if(finalMedia<6)
+//        System.out.println("REPROVADO");
+//        else if (finalMedia==6)
+//        System.out.println("PROVA MINERVA");
+//        else
+//        System.out.println("APROVADO");
+    }
+}
