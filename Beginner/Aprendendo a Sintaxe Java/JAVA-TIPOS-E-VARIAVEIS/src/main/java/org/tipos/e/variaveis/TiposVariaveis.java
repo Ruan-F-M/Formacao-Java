5fe2f8a8c059d8a1bf688e5c403c1edf7b2ca687
Ruan-F-M <ruanzinr1000@hotmail.com>
2023-01-11 23:37:44 -0300

    syntax pt5

---
+++ Beginner/Aprendendo a Sintaxe Java/JAVA-TIPOS-E-VARIAVEIS/src/main/java/org/tipos/e/variaveis/TiposVariaveis.java
@@ -0,0 +1,29 @@
+package org.tipos.e.variaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+//        byte idade = 123;
+//        short ano = 2021;
+//        int cep = 21070333;
+//        long cpf = 98765432109L; tem q ser terminado com L para não ser reconhecido como inteiro
+//        float pi = 3.14F; Colocar um F no final como tipo float para não pensar que é um double
+//        double salario = 1275.33;
+
+//        short numeroCurto = 1;
+//        int numeroNormal1 = numeroCurto;
+//        short numeroCurto2 = numeroNormal1;
+//        short numeroCurto2 = (short) numeroNormal1;
+//      denomina-se casting
+
+//        Variaveis x Constantes
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+    }
+}
