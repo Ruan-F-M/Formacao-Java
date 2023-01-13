82d768a5c7533dbc53ee9d8a1a2d52bdaa7cc3d9
Ruan-F-M <ruanzinr1000@hotmail.com>
2023-01-12 21:36:03 -0300

    syntax pt6

---
+++ Beginner/Aprendendo a Sintaxe Java/JAVA-OPERADORES/src/main/java/org/operadores/pt2/Operadores.java
@@ -0,0 +1,49 @@
+package org.operadores.pt2;
+
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+//        String nome = "RUAN";
+//        int idade = 23;
+//        double peso = 72;
+//        char sexo = 'M';
+//        boolean doadorOrgao = false;
+//        Date dataNascimente = new Date();
+//
+//        double soma = 10.5 + 15.7;
+//        int subtracão = 113 -25;
+//        int multiplicacao = 20 * 7;
+//        int divisao = 15 / 3;
+//        int modulo = 18 % 3;
+//        double resultado = (10 * 7) + (20/4);
+
+        //concatenação junção de duas palavras
+
+//        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        String concatenacao ="?";
+
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
