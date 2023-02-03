d436e58e1160292b09b65dfc0b20485ce8614764
Ruan-F-M <ruanzinr1000@hotmail.com>
2023-02-03 19:52:06 -0300

    validation pt3

--- Beginner/Controle Candidatos/src/main/java/candidatura/ProcessoSeletivo2.java
+++ Beginner/Controle Candidatos/src/main/java/candidatura/ProcessoSeletivo2.java
@@ -1,11 +1,8 @@
 package candidatura;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class ProcessoSeletivo {
+public class ProcessoSeletivo2 {
     public static void main(String[] args) {
-//        analisarCandidato(1900.0);
-//        analisarCandidato(2200.0);
-//        analisarCandidato(2000.0);
         selecaoCandidatos();
     }
 
@@ -32,15 +29,5 @@ public class ProcessoSeletivo {
 
     }
 
-//    static void analisarCandidato(double salarioPretendido) {
-//        double salarioBase = 2000.0;
-//        if (salarioBase > salarioPretendido) {
-//            System.out.println("LIGAR PARA O CANDIDATO");
-//        }else if (salarioBase == salarioPretendido)
-//            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
-//        else {
-//            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
-//        }
-//
-//    }
+
 }
