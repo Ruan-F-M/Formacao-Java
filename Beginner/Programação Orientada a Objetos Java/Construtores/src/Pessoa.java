4f02c1bc4f77dbd07cdaed3c5ca44611c10f875f
Ruan-F-M <ruanzinr1000@hotmail.com>
2023-02-06 21:56:12 -0300

    POO pt4

---
+++ Beginner/Programação Orientada a Objetos Java/Construtores/src/Pessoa.java
@@ -0,0 +1,33 @@
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Pessoa (String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+        public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String newEndereco) {
+        endereco = newEndereco;
+    }
+    //...
+    //setters de nome e cpf ?
+}
