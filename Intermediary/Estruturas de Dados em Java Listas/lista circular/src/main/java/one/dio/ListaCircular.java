a050e66a7d421825618ecdce7cec5137aa6c710a
Ruan França <ruanzinr1000@hotmail.com>
2023-06-19 22:59:16 -0300

    Lists pt22

--- Intermediary/Estruturas de Dados em Java Listas/lista circular/src/main/java/one/dio/ListaCircular.java
+++ Intermediary/Estruturas de Dados em Java Listas/lista circular/src/main/java/one/dio/ListaCircular.java
@@ -69,4 +69,17 @@ public class ListaCircular<T> {
         return this.tamanhoLista;
     }
 
+    @Override
+    public String toString() {
+        String strRetorno = "";
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0; i < size(); i++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        strRetorno += this.size() != 0 ? "(Retorna ao início)" : "[]";
+        return strRetorno;
+    }
 }
