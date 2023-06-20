e7f4d5bcc9dfd5fef6ab4e8eee457776fa3262a3
Ruan França <ruanzinr1000@hotmail.com>
2023-06-19 22:25:36 -0300

    Lists pt20

--- Intermediary/Estruturas de Dados em Java Listas/lista circular/src/main/java/one/dio/ListaCircular.java
+++ Intermediary/Estruturas de Dados em Java Listas/lista circular/src/main/java/one/dio/ListaCircular.java
@@ -1,4 +1,17 @@
 package one.dio;
 
-public class ListaCircular {
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> calda;
+    private int tamanhoLista;
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0 ? true : false;
+    }
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
 }
